762284f00a0e5db6185c00159b2f61f12f180c2b
rhea0110 <65884341+rhea0110@users.noreply.github.com>
2022-10-06 21:02:29 +0530

    done

--- pages/languages.jsx
+++ pages/languages.jsx
@@ -1,16 +1,138 @@
 import React from "react";
-import Head from "next/Head";
-import Navbar from "/components/Navbar";
+import Head from "next/head"
+import Link from "next/link"
 const Languages = () => {
-  
+    const line = [
+      {
+        lang: "English",
+        src: "eng"
+      },
+      {
+        lang: "Hindi",
+        src: "india"
+ 
+      },
+      {
+        lang: "Turkish",
+        src: "turk"
+ 
+      },
+      {
+        lang: "Hebrew",
+        src: "heb"
+ 
+      },
+      {
+        lang: "Arabic",
+        src: "arab"
+ 
+      },
+      {
+        lang: "Spanish",
+        src: "espa"
+ 
+      },
+      {
+        lang: "French",
+        src: "fre"
+ 
+      },
+      {
+        lang: "German",
+        src: "gem"
+ 
+      },
+      {
+        lang: "Russian",
+        src: "rus"
+ 
+      },
+      {
+        lang: "African",
+        src: "afn"
+ 
+      },
+      {
+        lang: "Chinese",
+        src: "chin"
+ 
+      },
+       {
+        lang: "Portugese",
+        src: "port"
+ 
+      },
+       {
+        lang: "Korean",
+        src: "kon"
+ 
+      },
+       {
+        lang: "Polish",
+        src: "pol"
+ 
+      },
+       {
+        lang: "Italian",
+        src: "it"
+ 
+      },
+       {
+        lang: "Japanese",
+        src: "japan"
+ 
+      },
+       {
+        lang: "Indonesian",
+        src: "indo"
+ 
+      },
+       {
+        lang: "Egyptian Arabic",
+        src: "eg"
+
+      },
+    ];
     return (
-        <>
-          <Head>
-      <title>Languages</title>
-    </Head>
-      <div className="h-full w-full text-white">
-        <Navbar/>
-      </div>
+      <>
+        <Head>
+          <title>Languages</title>
+        </Head>
+        <div>
+          <div className="flex flex-row">
+            <div className="text-red-500  text-3xl font-medium mt-[40px] ml-10">
+              Languages
+            </div>
+            <div className="ml-auto flex flex-row gap-x-5  mr-12 mt-[20px] h-fit">
+              <Link href="/alert">
+                <span className="text-md flex ml-auto h-fit pt-8 cursor-pointer flex-wrap gap-x-1 text-center py-1.5 px-1 text-red-600 border-b border-red-600 font-medium">
+                  Learn More
+                </span>
+              </Link>
+              <Link href="/settings">
+                <button className="text-md flex ml-auto h-fit pt-8 cursor-pointer flex-wrap gap-x-1 text-center py-1.5 px-1 text-red-600 border-b border-red-600 font-medium">
+                  Go Back
+                </button>
+              </Link>
+            </div>
+          </div>
+
+          <div>
+            <div type="circle" className="w-full h-full p-10 my-auto">
+              {line.map((lines, index) => (
+                <div
+                  key={index}
+                  className="text-white my-3 flex border-b border-red-500 flex-row text-lg text-left font-mono text-md"
+                >
+                  <div className="mt-2">
+                  {lines.lang.toUpperCase()}
+                  </div>
+                  <img src={`${lines.src}.png`} className={`w-12 ml-auto`} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </>
     );
 }
---
+++ pages/logout.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Logout = () => {
+    return (
+      <div className="w-full  flex-col justify-center items-center flex p-20 h-screen bg-black">
+        <div>
+          <h1 className="w-full h-full  font-mono text-5xl justify-center items-center flex">
+            {"Thankyou for watching my project!".toUpperCase()}
+          </h1>
+        </div>
+        <div className=" mt-12 font-mono text-xl ">
+          {"Click on the link to visit my "}
+          <a
+            className="text-red-500 text-xl underline"
+            href="https://rheajain.netlify.app/"
+          >
+            Live Profile
+          </a>
+        </div>
+        <div className=" mt-6 font-mono text-xl ">
+          {"You can also visit my linkedIn profile "}
+          <a
+            className="text-red-500 text-xl underline"
+            href="https://www.linkedin.com/in/rhea-jain-2462a3189/"
+          >
+            Rhea Jain
+          </a>
+        </div>
+      </div>
+    );
+}
+
+export default Logout;
